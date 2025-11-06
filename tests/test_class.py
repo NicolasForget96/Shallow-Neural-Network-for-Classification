044cfcad2a6a6b2fb23f5c0692699bf8d163ba03
NicolasForget96 <238455702+NicolasForget96@users.noreply.github.com>
2025-11-06 13:19:39 +0100

    Added unit test for Adam

--- tests/test_class.py
+++ tests/test_class.py
@@ -9,7 +9,7 @@ from visualisation import visualize_classifier
 from splitter import transform_to_numpy
 from scaler import Scaler
 from neural_network import NeuralNetwork
-from optimizers import GradientDescent
+from optimizers import GradientDescent, AdamOptimizer
 import numpy as np
 
 class TestClass:
@@ -29,7 +29,7 @@ class TestClass:
         assert accuracy == pytest.approx(93.54, 0.01)
 
 
-    def test_roughly_decreasing_costs(self):
+    def test_decreasing_costs_GD(self):
         data, features, target = load_data_tsunami()
         X, y = transform_to_numpy(data, features, target)
 
@@ -41,3 +41,19 @@ class TestClass:
         nn.fit(X_norm, y)
 
         assert np.sum(nn.costs[1:] >= nn.costs[:-1]) == 0
+
+
+    def test_final_cost_ADAM(self):
+        data, features, target = load_data_tsunami()
+        X, y = transform_to_numpy(data, features, target)
+
+        sclr = Scaler()
+        sclr.fit(X)
+        X_norm = sclr.transform(X)
+
+        nn = NeuralNetwork(nb_units=100, nb_it=10000, output='softmax', opt=AdamOptimizer())
+        final_cost = nn.fit(X_norm, y)
+        accuracy = nn.get_accuracy(X_norm, y)
+
+        assert final_cost == pytest.approx(0.0539, 0.001)
+        assert accuracy == pytest.approx(97.61, 0.01)
